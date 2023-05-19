14d0da5b4a904e83dd51cf5e40d70afabd4bf808
Kenta Murata <mrkn@mrkn.jp>
2023-05-19 20:25:45 +0900

    Implement Message.previous_message

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,19 @@ class Message < ApplicationRecord
   belongs_to :user
 
   has_one :query
+
+  def thread_messages
+    Message.where(slack_thread_ts: self.slack_thread_ts).order(slack_ts: :asc).to_a
+  end
+
+  def previous_message
+    messages = thread_messages
+    return nil if messages.length == 1
+
+    messages.each_cons(2) do |a, b|
+      return a if b.slack_ts == self.slack_ts
+    end
+
+    nil
+  end
 end
